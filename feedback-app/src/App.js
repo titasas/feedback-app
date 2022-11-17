a2cf92d0b7602d33af2fac598ff75eef81b2eae6
titasas <titas.petrauskas@gmail.com>
2022-11-17 10:58:15 +0200

    Add statistics for overall feedbacks

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Header } from "./components/Header";
 import { FeedbackList } from "./components/FeedbackList";
 import { FeedbackData } from "./data/FeedbackData";
+import { FeedbackStats } from "./components/FeedbackStats";
 
 export function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -16,6 +17,7 @@ export function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
