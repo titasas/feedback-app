59236f98eb23b5f8af62eaa548e6b29e1a9b2a05
titasas <titas.petrauskas@gmail.com>
2022-11-17 11:36:36 +0200

    Customize the button component

--- feedback-app/src/Components/FeedbackForm.jsx
+++ feedback-app/src/Components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Card } from "./shared/Card";
+import { Button } from "./shared/Button";
 
 export function FeedbackForm() {
   const [text, setText] = useState("");
@@ -20,7 +21,7 @@ export function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <button type="submit">Send</button>
+          <Button type="submit">Send</Button>
         </div>
       </form>
     </Card>
